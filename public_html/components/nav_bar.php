31f0e10a2c486797e33f01a78504240870d1496b
Suee18 <121303661+Suee18@users.noreply.github.com>
2024-10-14 14:07:42 +0300

    global css , linking landing to index, nav bar

---
+++ public_html/components/nav_bar.php
@@ -0,0 +1,53 @@
+<link rel="stylesheet" href="css/nav_bar.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<nav class="navbar">
+    <div class="container">
+
+        <ul class="nav-links">
+            <li>
+                <a href="#">
+                    <span class="material-icons">
+                        search
+                    </span>
+                    </a>
+            </li>
+
+            <li>
+                <a
+                    href="#"
+                    class="listElementHidden_navBar">
+                    Search for cars
+                </a>
+            </li>
+
+
+            <li>
+                <a
+                    href="#"
+                    class="listElement_navBar">
+                    Compare cars
+                </a>
+            </li>
+
+            <li>
+                <a href="#"
+                    class="listElementLogo_navBar"
+                   >
+                    Apex
+                </a>
+            </li>
+
+
+           
+
+            <li>
+                <a href="#"
+                    class="listElement_login_navBar"
+                    id="listElement_login_navBar">
+                    log in
+                </a>
+            </li>
+
+        </ul>
+    </div>
+</nav>
---
+++ public_html/index.php
@@ -0,0 +1,4 @@
+<?php
+// Main entry point for the site
+
+require_once '../app/views/landing_page.php';
