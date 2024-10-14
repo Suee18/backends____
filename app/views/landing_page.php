89c2af26ec50f89a5250fffee799f92311b8edb3
Suee18 <121303661+Suee18@users.noreply.github.com>
2024-10-14 17:46:46 +0300

    video lp

--- app/views/landing_page.php
+++ app/views/landing_page.php
@@ -1,81 +1,84 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="css/landing_page.css">
+    <link rel="stylesheet" href="css/landing_page.css">
     <title>Landing Page</title>
 </head>
+
 <body>
- <?php include './components/nav_bar.php'; ?> 
+    <?php include './components/nav_bar.php'; ?>
 
 
     <div class="slideShowContainer_lp">
 
-<!-- Slide 1: Video Background -->
-<div class="slide" id="slide1">
-    <video autoplay muted loop class="slide-bg">
-    <source src="media/BMWM5CS.mp4" type="video/mp4">
-    </video>
+        <!-- Slide 1: Video Background -->
+        <div class="slide" id="slide1">
+            <div class="videoBG_lp">
+                <video autoplay muted loop class="slide-bg">
+                    <source src="media/BMWM5CS.mp4" type="video/mp4">
+                </video>
+            </div>
+            <div class="text-overlay">
+                <p class="slide1Title_lp">
+                    you can't really hide who you are</p>
+                <p class="slide1paragraph_lp">
+                    Take the test now, and find your match on wheels.</p>
+            </div>
+        </div>
 
-    <div class="text-overlay">
-        <p class="slide1Title_lp">
-            you can't really hide who you are</p>
-        <p class="slide1paragraph_lp">
-            Take the test now, and find your match on wheels.</p>
+
+
+        <!-- Slide 2: Image Background -->
+
+        <div class="slide" id="slide2">
+            <img src="media/thisOrThat.png" class="slide-bg" alt="Image Background">
+            <div class="text-overlay">
+                <p
+                    class="slide2Title_lp">
+                    This or that?</p>
+                <p class="slide2paragraph_lp">Compare between any two car Models</p>
+            </div>
+
+        </div>
+
+        <!-- Slide 3:  -->
+        <div class="slide" id="slide3">
+            <img src="path_to_your_image2.jpg" class="slide-bg" alt="Image Background">
+            <div class="text-overlay">
+                <h2>Ready, set, Turbo! </h2>
+                <p>Talk now to Turbo, the chatbot</p>
+            </div>
+        </div>
+
+        <!-- Slide 4: -->
+        <div class="slide" id="slide4">
+            <img src="path_to_your_image3.jpg" class="slide-bg" alt="Image Background">
+            <div class="text-overlay">
+                <h2>Discover latest news, and read authentic reviews.</h2>
+            </div>
+        </div>
+
+
+
+        <!-- Dots for navigation -->
+        <div class="dots">
+            <span class="dot" onclick="showSlideOnClick(1)"></span>
+            <span class="dot" onclick="showSlideOnClick(2)"></span>
+            <span class="dot" onclick="showSlideOnClick(3)"></span>
+            <span class="dot" onclick="showSlideOnClick(4)"></span>
+        </div>
     </div>
+
+<div class="landingPage_part2">
+    <P>PART 2 GOES HERE </P>
 </div>
-
-
-
-<!-- Slide 2: Image Background -->
-
-<div class="slide" id="slide2">
-    <img src="media/thisOrThat.png" class="slide-bg" alt="Image Background">
-    <div class="text-overlay">
-        <p 
-        class="slide2Title_lp">
-        This or that?</p>
-        <p class="slide2paragraph_lp">Compare between any two car Models</p>
-    </div>
-
-</div>
-
-<!-- Slide 3:  -->
-<div class="slide" id="slide3">
-    <img src="path_to_your_image2.jpg" class="slide-bg" alt="Image Background">
-    <div class="text-overlay">
-        <h2>Ready, set, Turbo! </h2>
-        <p>Talk now to Turbo, the chatbot</p>
-    </div>
-</div>
-
-<!-- Slide 4: -->
-<div class="slide" id="slide4">
-    <img src="path_to_your_image3.jpg" class="slide-bg" alt="Image Background">
-    <div class="text-overlay">
-        <h2>Discover latest news, and read authentic reviews.</h2>
-    </div>
-</div>
-
-
-
-<!-- Dots for navigation -->
-<div class="dots">
-    <span class="dot" onclick="showSlideOnClick(1)"></span>
-    <span class="dot" onclick="showSlideOnClick(2)"></span> 
-      <span class="dot" onclick="showSlideOnClick(3)"></span>
-    <span class="dot" onclick="showSlideOnClick(4)"></span>
-</div>
-</div>
- 
-
-
-<script src="js/landing_page.js"></script>
+    <script src="js/landing_page.js"></script>
 
 
 
 </body>
+
 </html>
-
-
