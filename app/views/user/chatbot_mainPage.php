f2a768342939d1409eb32f3c965a0c8e5f2e9804
Alaa Waleed <aw827948@gmail.com>
2024-10-14 19:10:39 +0300

    First (top) part of the chatbot main page

--- app/views/user/chatbot_mainPage.php
+++ app/views/user/chatbot_mainPage.php
@@ -5,8 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Turbo</title>
     <link rel="stylesheet" href="../../../public_html/css/chatbot_Main.css">
+    <script src="../../../public_html/js/chatbot_MainPage.js"></script>
 </head>
 <body>
-    <p>123... css link</p>
+
+<!-- Top of the Page -->
+<div class="div">
+        <img id="chatbot" class="chatbot" src="../../../public_html/media/chatbot2.png" alt="Chatbot Icon">
+        <p id="qoute" class="qoute">Turbo here! Let's shift gears and get your<br> questions answered in a flash!</p>
+        <button id="chat_btn" class="chat_btn">Start A Chat</button>
+    </div>
+
+    
+<!-- Second part -->
+ 
 </body>
 </html>
