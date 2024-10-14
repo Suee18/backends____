9b91a9ffa73b9a117f78ca2d337022b8da87bec5
Suee18 <121303661+Suee18@users.noreply.github.com>
2024-10-14 17:13:18 +0300

    chatbot css integration

--- app/views/user/chatbot_mainPage.php
+++ app/views/user/chatbot_mainPage.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/chatbot_Main.css"> 
     <title>Turbo</title>
+    <link rel="stylesheet" href="../../../public_html/css/chatbot_Main.css">
 </head>
 <body>
-
+    <p>123... css link</p>
 </body>
-</html>     
+</html>
