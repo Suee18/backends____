f2d765b17420121bc28ffc43c4c7c208d8f70d2d
FadyNF <fadynf05@gmail.com>
2024-10-17 15:52:29 +0300

    Added basic car comparison structure

---
+++ app/views/car_comparison.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public_html/css/car_comparison.css">
+    <link rel="stylesheet" href="../../public_html/css/global_styles.css">
+    <link rel="stylesheet" href="../../public_html/css/nav_bar.css">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=New+Amsterdam&display=swap" rel="stylesheet">
+    <title>Compare Cars Now!</title>
+    <script src=""></script>
+</head>
+
+<body>
+    <?php include '../../public_html/components/nav_bar.php'; ?>
+    <div id="comparing-cars-banner">
+        <h1>Compare Cars Models</h1>
+    </div>
+</body>
+
+</html>
