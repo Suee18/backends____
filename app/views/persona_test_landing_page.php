9fa0976f38dad182f53234e80b3891b3fee18bf5
FadyNF <fadynf05@gmail.com>
2024-10-20 12:55:33 +0300

    added a landing page for the persona quiz

---
+++ app/views/persona_test_landing_page.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=New+Amsterdam&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+        rel="stylesheet">
+
+    <title>Car Recommendation App</title>
+    <link rel="stylesheet" href="../../public_html/css/persona_test_landing_page.css">
+    <link rel="stylesheet" href="../../public_html/css/nav_bar.css">
+</head>
+
+<body>
+
+    <?php include '../../public_html/components/nav_bar.php'; ?>
+    <div class="landing-page">
+        <div class="content">
+            <h1>Discover Your Perfect Car Persona.</h1>
+            <p>Ready to find the car that fits your lifestyle? Take our quick quiz to reveal your unique car persona and
+                get personalized recommendations. Whether you're into speed, tech, eco-friendliness, or something else,
+                we’ve got you covered.</p>
+            <a href="#" class="cta-button">Find My Car</a>
+        </div>
+        <div class="image-container">
+            <img src="../../public_html/media/Persona_Test_Images/Landing/fast-red-car-road-back-view.jpg" alt="Car" />
+        </div>
+    </div>
+</body>
+
+</html>
