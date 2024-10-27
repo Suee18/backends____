136d2adec92e722640444c3c67d864f08d3d6461
FadyNF <fadynf05@gmail.com>
2024-10-27 15:58:54 +0300

    Refactor persona test CSS styles and add animated button for retaking the test

--- app/views/user/persona.php
+++ app/views/user/persona.php
@@ -16,8 +16,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=New+Amsterdam&display=swap" rel="stylesheet">
     <link
         href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
-        rel="stylesheet"
-    >
+        rel="stylesheet">
 
     <script src="../../../public_html/js/persona.js"></script>
     <title>Car Persona Results</title>
@@ -145,6 +144,21 @@
             </div>
         </div>
     </div>
+
+    <button class="animated-button open-modal-button">
+        <svg xmlns="http://www.w3.org/2000/svg" class="arr-2" viewBox="0 0 24 24">
+            <path
+                d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z">
+            </path>
+        </svg>
+        <span class="text">Retake the test</span>
+        <span class="circle"></span>
+        <svg xmlns="http://www.w3.org/2000/svg" class="arr-1" viewBox="0 0 24 24">
+            <path
+                d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z">
+            </path>
+        </svg>
+    </button>
 </body>
 
 </html>
